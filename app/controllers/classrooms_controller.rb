d4b3a655cdc628d739f3d64ba8323d39f17daf75
Alex Chou <chou.alexander@berkeley.edu>
2015-03-10 02:44:38 -0300

    add in simple view to see prefilled link

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -10,4 +10,14 @@ class ClassroomsController < ApplicationController
 		redirect_to teacher_path(@teacher)
 	end
 
+  def create_link
+    require 'bitly'
+    teacher = session[:teacher]
+    classroom = Classroom.find(params[:id])
+    survey_url = %Q{https://docs.google.com/a/bizworld.org/forms/d/192Eqjup01aq3zAvTS-P9wFTCJZChaGNHDAusrUKMd6o/viewform?entry.1402717777=#{}&entry.363323870=#{}&entry.819974938=GHI&entry.1920272073=JKL&entry.942453101&entry.1463927784}
+
+    bitly = Bitly.new('cs169bizworld', 'R_b3491d455043441192ad7e645659cbd0')
+    @prefilledURL = bitly.shorten(survey_url).short_url
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ BizworldApp::Application.routes.draw do
   
   resources :teachers do
     resources :classrooms
+    get '/classrooms/:id/create_link' => 'classrooms#create_link'
   end
 
 end
