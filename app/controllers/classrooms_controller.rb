37a3f06a8d7429d20f618ba0dad18ded702a6edc
Chatdog <david.ni@berkeley.edu>
2015-03-10 03:15:06 -0300

    before alex's bugfixes

--- app/controllers/classrooms_controller.rb
+++ app/controllers/classrooms_controller.rb
@@ -22,7 +22,7 @@ class ClassroomsController < ApplicationController
     base_url = %Q{https://docs.google.com/a/bizworld.org/forms/d/192Eqjup01aq3zAvTS-P9wFTCJZChaGNHDAusrUKMd6o/viewform?}
     school_field = %Q{&entry.363323870=} + school
     teacher_field = %Q{&entry.819974938=} + teacher_name
-    city_field = %Q{entry.1920272073=} + city
+    city_field = %Q{&entry.1920272073=} + city
     state_name = %Q{&entry.1920272073=} + state
     tail_url = %Q{&entry.942453101&entry.1463927784}
     survey_url = base_url + school_field + teacher_field + city_field + state_name + tail_url
