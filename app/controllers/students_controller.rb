df1a4c54d86daa2e7ac0becb06c13e4bd055f4df
Kenneth Le <Kenneth@Kenneths-MacBook-Pro.local>
2015-04-02 21:45:42 -0700

    table frontend

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,9 +3,8 @@ class StudentsController < ApplicationController
   before_filter :authenticate_teacher!
 
   def show
-    @student = Student.find(params[:id])
-    #To be changed once login is implemented
-    @teacher = Teacher.first
+    classroom = Classroom.find(params[:classroom_id])
+    @student = classroom.students.where(:id => params[:id])[0]
   end
 
 end
