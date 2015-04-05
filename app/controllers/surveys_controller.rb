a45668f4e92ad615c81fbb9c36217f95ef77bfb8
Aneesh Prasad <aneeshprasad1@gmail.com>
2015-04-05 15:40:47 -0700

    In progress: Fixing student table queries and parameters passed in through url

--- app/controllers/surveys_controller.rb
+++ app/controllers/surveys_controller.rb
@@ -1,13 +1,16 @@
 class SurveysController < ApplicationController
 
   def create
+    if !params.include?(:responses)
+      return
+    end
     responses = params[:responses]
     first_name = responses["First Name"]
     last_name = responses["Last Name"]
     classroom = responses["Classroom"]
     survey_type = responses["Survey Type"]
 
-    student = Student.find_by_classroom_and_first_name_and_last_name(classroom, first_name, last_name)
+    student = Student.joins(:classrooms).find(:student_first_name => first_name, :student_last_name => last_name, :classroom_id => classroom)
     survey = student.survey.create :survey_type => survey_type
     survey.populate responses
     survey.grade
