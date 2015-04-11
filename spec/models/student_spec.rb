ace16301a5835ac9201ffc79953a20bfd8760ff2
Alex Chou <chou.alexander@berkeley.edu>
2015-04-11 20:38:32 -0300

    fix student model bug, add more tests for student model spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -2,10 +2,10 @@ require 'spec_helper'
 
 describe Student do
   before (:each) do
-    @student = Student.create()
+    @student = Student.create(first_name: "Alice", last_name: "Bob")
   end
 
-  describe "#get_survey" do
+  describe "#get_survey and #get_survey_score" do
     it 'should return the pre result' do
       pre_survey = @student.surveys.create(:survey_type => 'pre', :score => 25.4)
       @student.get_survey('pre').should == pre_survey
@@ -26,4 +26,23 @@ describe Student do
     end
   end
 
+  describe "#name" do
+    it 'should return the student\'s first and last name' do
+      @student.name.should == "#{@student.first_name} #{@student.last_name}"
+    end
+  end
+
+  describe "#create" do
+    it 'should not allow a student without a first and last name to be created' do
+      Student.new().should_not be_valid
+    end
+
+    it 'should not allow a student without a last name to be created' do
+      Student.new(first_name: "Alice").should_not be_valid
+    end
+
+    it 'should not allow a student without a first name to be created' do
+      Student.new(last_name: "Bob").should_not be_valid
+    end
+  end
 end
