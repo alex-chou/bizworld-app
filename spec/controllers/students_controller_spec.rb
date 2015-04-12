602350bbd903a12643480a1ecfb80d1af93c7e97
Alex Chou <chou.alexander@berkeley.edu>
2015-04-12 18:06:04 -0300

    working on setting up devise login for rspec

--- spec/controllers/students_controller_spec.rb
+++ spec/controllers/students_controller_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe StudentsController do
+  describe '#show' do
+    describe 'logged in teacher' do
+    end
+
+    describe 'not logged in' do
+      it 'should redirect to log in page' do
+      end
+    end
+  end
 
 end
--- spec/controllers/teachers_controller_spec.rb
+++ spec/controllers/teachers_controller_spec.rb
@@ -2,4 +2,30 @@ require 'spec_helper'
 
 describe TeachersController do
 
+  describe 'teacher logged in' do
+    before (:each) do
+      @teacher = Teacher.create!(city: "Berkeley", email: "bob@gmail.com", name: "Bob Alice", school_name: "UC Berkeley", state: "CA", username: "bob", password: "password")
+      sign_in :teacher, @teacher
+    end
+
+    describe '#index' do
+      it 'should show the teacher their own page' do
+        get 'index'
+        response.should redirect_to(teacher_path(@teacher.id))
+      end
+
+      it 'should redirect the teacher to their own page' do
+        get 'index', :id => 0
+        response.should redirect_to(teacher_path(@teacher.id))
+      end
+    end
+  end
+
+  describe 'teacher not logged in' do
+    it 'should redirect to the login page' do
+      get 'index'
+      response.should redirect_to(new_teacher_session_path)
+    end
+  end
+
 end
