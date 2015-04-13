e42b6f2876b02fafc3773ea6a7597b37eaac0197
Alex Chou <chou.alexander@berkeley.edu>
2015-04-13 02:49:20 -0300

    use factory girl in student spec

--- spec/models/student_spec.rb
+++ spec/models/student_spec.rb
@@ -1,47 +1,51 @@
-require 'spec_helper'
+require "spec_helper"
 
 describe Student do
   before (:each) do
-    @student = Student.create(first_name: "Alice", last_name: "Bob")
+    @student = FactoryGirl.create :student
   end
 
   describe "#get_survey and #get_survey_score" do
-    it 'should return the pre result' do
-      pre_survey = @student.surveys.create(:survey_type => 'pre', :score => 25.4)
-      @student.get_survey('pre').should == pre_survey
-      @student.get_survey_score('pre').should == pre_survey.score
+    it "should return the pre result" do
+      pre_survey = FactoryGirl.create :survey, :student => @student
+      @student.get_survey("pre").should == pre_survey
+      @student.get_survey_score("pre").should == pre_survey.score
+      @student.get_survey("post").should == nil
+      @student.get_survey_score("post").should == "N/A"
     end
 
-    it 'should return the post result' do
-      post_survey = @student.surveys.create(:survey_type => 'post', :score => 38.4)
-      @student.get_survey('post').should == post_survey
-      @student.get_survey_score('post').should == post_survey.score
+    it "should return the post result" do
+      post_survey = FactoryGirl.create :survey, :student => @student, :survey_type => "post"
+      @student.get_survey("pre").should == nil
+      @student.get_survey_score("pre").should == "N/A"
+      @student.get_survey("post").should == post_survey
+      @student.get_survey_score("post").should == post_survey.score
     end
 
-    it 'should return N/A with no survey result' do
-      @student.get_survey('pre').should == nil
-      @student.get_survey_score('pre').should == "N/A"
-      @student.get_survey('post').should == nil
-      @student.get_survey_score('post').should == "N/A"
+    it "should return N/A with no survey result" do
+      @student.get_survey("pre").should == nil
+      @student.get_survey_score("pre").should == "N/A"
+      @student.get_survey("post").should == nil
+      @student.get_survey_score("post").should == "N/A"
     end
   end
 
   describe "#name" do
-    it 'should return the student\'s first and last name' do
+    it "should return the student\"s first and last name" do
       @student.name.should == "#{@student.first_name} #{@student.last_name}"
     end
   end
 
   describe "#create" do
-    it 'should not allow a student without a first and last name to be created' do
+    it "should not allow a student without a first and last name to be created" do
       Student.new().should_not be_valid
     end
 
-    it 'should not allow a student without a last name to be created' do
+    it "should not allow a student without a last name to be created" do
       Student.new(first_name: "Alice").should_not be_valid
     end
 
-    it 'should not allow a student without a first name to be created' do
+    it "should not allow a student without a first name to be created" do
       Student.new(last_name: "Bob").should_not be_valid
     end
   end
--- spec/support/factory_girl.rb
+++ spec/support/factory_girl.rb
@@ -4,8 +4,8 @@ end
 
 FactoryGirl.define do
   factory :teacher do
-    name "Alice Bob"
-    email "bob@yahoo.com"
+    name "Alice Private"
+    email "private@yahoo.com"
     password "password"
     password_confirmation "password"
     city "Berkeley"
@@ -13,11 +13,19 @@ FactoryGirl.define do
     state "CA"
   end
 
-  factory :survey do
-    survey_type "pre"
-  end
-
   factory :classroom do
     association :teacher, :factory => :teacher
+  end
+
+  factory :student do
+    classrooms {[FactoryGirl.create(:classroom)]}
+    first_name "Alice"
+    last_name "Private"
+  end
+
+  factory :survey do
+    association :student, :factory => :student
+    survey_type "pre"
+    score 76.4
   end
 end
