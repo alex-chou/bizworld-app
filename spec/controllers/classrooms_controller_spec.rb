1e364da1fe991424b3f9f825c95519e0302e0d16
Alex Chou <chou.alexander@berkeley.edu>
2015-04-13 03:35:08 -0300

    classroom controller spec

--- spec/controllers/classrooms_controller_spec.rb
+++ spec/controllers/classrooms_controller_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
 describe ClassroomsController do
+  before (:each) do
+    sign_in_valid_teacher
+  end
 
+  describe '#create' do
+    it 'should create a new classroom tied to the teacher' do
+      post "create", :teacher_id => @teacher.id
+      response.should redirect_to(teacher_path(@teacher))
+      Classroom.all[0].teacher.should == @teacher
+    end
+  end
 end
