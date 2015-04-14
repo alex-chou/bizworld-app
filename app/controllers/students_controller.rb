f41738275375a3f683e614eeca6dbe6e2925c243
saasbook <saasbook@saasbook.(none)>
2015-04-13 21:12:31 -0300

    Spreadsheet upload working for a wider variety of header labels

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,9 +9,6 @@ class StudentsController < ApplicationController
   end
 
 
-  # importing from an excel file currently works only if 
-  # the first column is labeled with the header "first_name", and 
-  # the second column is labeled with the header "last_name".
   def import
     Student.import(params[:file], session["classroom_id"])
     redirect_to root_url, notice: "Students added."
