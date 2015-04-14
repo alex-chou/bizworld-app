434dca0ac84e228fb6eaf26112ccd8e50fabeefb
Chatdog <david.ni@berkeley.edu>
2015-04-13 21:00:32 -0300

    Added validations to classrooms, teachers, and surveys and created migration to remove usernames from teachers

---
+++ db/migrate/20150413234511_remove_username_from_teachers.rb
@@ -0,0 +1,9 @@
+class RemoveUsernameFromTeachers < ActiveRecord::Migration
+  def up
+    remove_column :teachers, :username
+  end
+
+  def down
+    add_column :teachers, :username, :string
+  end
+end
