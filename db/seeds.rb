474bd114ee056924215609546575e66861d2322c
Kenneth Le <Kenneth@airbears2-10-142-56-115.airbears2.1918.berkeley.edu>
2015-04-15 13:31:34 -0700

    add city_name to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@
     classroom = teacher.classrooms.create!(name: "MWF#{count1} #{count1}-#{count1 + 1} PM", class_type: count1.to_s, program: "bizworld")
     12.times do |count2|
       student = classroom.students.create!(first_name: "Eve#{count2}", last_name: "Casey", age: (count0+count1+count2).to_s, gender: "female", ethnicity: "Decline to Respond",
-                                           school_name: "Cal Elementary", state: "CA", teacher_name: "Armando Fox", grade: 4)
+                                           city_name: "Berkeley", school_name: "Cal Elementary", state: "CA", teacher_name: "Armando Fox", grade: 4)
       if count2 % 2 == 0
         student.surveys.create(:survey_type => 'pre', :score => count2 * 2.5)
       end
